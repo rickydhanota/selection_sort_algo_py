de524a69020d92b5b8ea41d19cce0aa5c1c21fb6
Ricky <rickdhanota@gmail.com>
2020-10-17 22:01:56 -0700

    initial commit

---
+++ selection_sort.py
@@ -0,0 +1,4 @@
+# Selection Sort
+# Write a function that takes in an array of integers and returns a sorted version of that array. Use the selection ort algorithm method to sort the array
+#Array = [8, 5, 2, 9, 5, 6, 3]
+# ouput = [2, 3, 5, 5, 6, 8, 9]
