15a2b2447e2f75bccb541854639577001affe01d
Ricky <rickdhanota@gmail.com>
2020-10-17 22:14:19 -0700

    solved

--- selection_sort.py
+++ selection_sort.py
@@ -4,6 +4,17 @@
 # ouput = [2, 3, 5, 5, 6, 8, 9]
 
 def selectionSort(array):
-    pass
+    currentIdx = 0
+    while currentIdx < len(array) - 1:
+        smallestIdx = currentIdx
+        for i in range(currentIdx + 1, len(array)):
+            if array[smallestIdx] > array[i]:
+                smallestIdx = i
+        swap(currentIdx, smallestIdx, array)
+        currentIdx += 1
+    return array
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
 
 print(selectionSort([8, 5, 2, 9, 5, 6, 3]))
